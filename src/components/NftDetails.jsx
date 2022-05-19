69298601d92df99515434b389a83f5588a602761
izhanfaridi <izhanfaridi@gmail.com>
2022-05-19 15:57:16 +0500

    improved details design

--- src/components/NftDetails.jsx
+++ src/components/NftDetails.jsx
@@ -2,33 +2,33 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import nfts from "./Data";
 import Navbar from "./Navbar";
-import {BsHeart} from 'react-icons/bs'
-import {IoWalletOutline} from 'react-icons/io5'
-import {BsBookmarkHeart} from 'react-icons/bs'
+import { BsHeart } from 'react-icons/bs'
+import { IoWalletOutline } from 'react-icons/io5'
+import { BsBookmarkHeart } from 'react-icons/bs'
 
 const NftDetails = () => {
   const location = useLocation();
-  const [nft , setNFT] = useState({});
+  const [nft, setNFT] = useState({});
   const id = location.pathname.split("/")[2];
- 
+
   useEffect(() => {
-    nfts.map((item)=>{
-        if(item.id == id){
-            setNFT(item);
-        }
+    nfts.map((item) => {
+      if (item.id == id) {
+        setNFT(item);
+      }
     })
   }, [id])
-  
+
 
   return (
     <>
       <Navbar />
       <div className="gradient-css-div-details h-screen">
         <br />
-        <div className="flex justify-around">
-          <div className="ring ring-1 ring-black rounded-md">
+        <div className="flex justify-evenly items-start">
+          <div className="w-96 ring ring-1 ring-black rounded-md flex flex-col justify-center items-end">
             <img
-              className="h-96 rounded-t-md"
+              className="h-96 w-96 rounded-t-md"
               src={nft.image}
               alt={nft.title}
             />
@@ -39,12 +39,12 @@ const NftDetails = () => {
               </div>
             </div>
           </div>
-          <div className="">
-            <div className="ring ring-1 ring-black rounded-t-md px-4 py-2 bg-white w-full">
+          <div className="flex-col  w-8/12    	">
+            <div className="ring ring-1 ring-black rounded-t-md px-4 py-2 details-div w-full">
               <p className="text-md font-medium text-gray-500">{nft.creator}</p>
               <p className="py-5 text-4xl font-bold">{nft.title}</p>
             </div>
-            <div className="rounded-b-md ring ring-1 ring-gray-500 px-4 py-2 bg-slate-50 w-full">
+            <div className="rounded-b-md ring ring-1 ring-black px-4 py-2 details-div w-full">
               <p className="text-md font-medium text-gray-700 mt-2">Current Price</p>
               <p className="text-4xl font-bold my-5">
                 <svg
@@ -56,8 +56,8 @@ const NftDetails = () => {
                 </svg>
                 {nft.price}
               </p>
-              <button className="bg-cyan-500 py-1 w-40 h-10 rounded-lg mb-4 mt-2 text-lg font-semibold text-white hover:bg-cyan-600"><IoWalletOutline className="inline w-6 h-6 mr-2 mb-1"/>Buy Now</button>
-              <button className="bg-white py-1 w-56 h-10 rounded-lg mb-4 ml-3 mt-2 text-lg font-semibold ring ring-1 ring-cyan-500 hover:ring-cyan-600 hover:ring-2"><BsBookmarkHeart className="inline w-6 h-6 mr-2 mb-1"/>Add To Favourites</button>
+              <button className="bg-cyan-500 py-1 w-40 h-10 rounded-lg mb-4 mt-2 text-lg font-semibold text-white hover:bg-cyan-600"><IoWalletOutline className="inline w-6 h-6 mr-2 mb-1" />Buy Now</button>
+              <button className="bg-white py-1 w-56 h-10 rounded-lg mb-4 ml-3 mt-2 text-lg font-semibold ring ring-1 ring-cyan-500 hover:ring-cyan-600 hover:ring-2"><BsBookmarkHeart className="inline w-6 h-6 mr-2 mb-1" />Add To Favourites</button>
             </div>
           </div>
         </div>
